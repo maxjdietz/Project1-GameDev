474c3fb244eff2d1c49bd1679f0188ce9d440ad9
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2025-09-08 15:22:47 -0600

    Initial commit

---
+++ Changes_Player/game.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+func _ready():
+	spawn_mob()
+	spawn_mob()
+	spawn_mob()
+	spawn_mob()
+
+func spawn_mob():
+	var new_mob = preload("res://Changes_Player/SlimeGuy.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob)
+
+
+func _on_timer_timeout() -> void:
+	spawn_mob()
+
+
+func _on_player_health_depleted() -> void:
+	%GameOverScreen.visible = true
+	get_tree().paused = true
---
+++ Changes_Player/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+var health = 100.0
+const DAMAGE_RATE = 20.0
+
+signal health_depleted
+
+
+func _physics_process(delta):
+	var direction = Input.get_vector("move_left", "move_right", 
+	"move_up", "move_down")
+	velocity = direction * 600
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		%HappyBoo.play_walk_animation()
+	else:
+		%HappyBoo.play_idle_animation()
+		
+	
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		print("TESTING")
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
+		if health <= 0.0:
+			print("TESTING")
+			health_depleted.emit()
+			
