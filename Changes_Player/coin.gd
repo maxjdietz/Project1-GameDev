49200afea96ffccfa4bcaec5242fd63c7e3ee61d
maxjdietz <111810767+maxjdietz@users.noreply.github.com>
2025-09-23 08:59:11 -0600

    P3-1

--- Changes_Player/coin.gd
+++ Changes_Player/coin.gd
@@ -3,8 +3,15 @@ extends Node2D
 signal coinGot
 
 
+func _ready():
+	play_spin( )
+
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		print("COIN")
 		coinGot.emit(2)
 		queue_free()
+		
+func play_spin():
+	%AnimationPlayer.play("Coin Spin")
